bc2acba3998b3dbda9c501d9c518b9ae7237366b
Dmitriy Panfilyonok <dmitriy.panfilyonok@gmail.com>
2022-11-03 05:38:43 +0300

    Add basic functionality

--- src/SimpleCRUD/Controllers/HomeController.cs
+++ src/SimpleCRUD/Controllers/HomeController.cs
@@ -1,26 +1,66 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using SimpleCRUD.Models;
+﻿namespace SimpleCRUD.Controllers;
 
-namespace SimpleCRUD.Controllers;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
 
 public class HomeController : Controller
 {
+    private readonly ApplicationContext _context;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ApplicationContext context, ILogger<HomeController> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Users.ToListAsync());
+    }
+
+    public IActionResult Create()
     {
         return View();
     }
-
-    public IActionResult Privacy()
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(User user)
     {
-        return View();
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) return NotFound();
+        
+        var user = new User { Id = id.Value };
+        _context.Entry(user).State = EntityState.Deleted;
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+    
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null) return NotFound();
+        
+        var user = await _context.Users.FirstOrDefaultAsync(p=> p.Id == id);
+        
+        if (user == null) return NotFound();
+        
+        return View(user);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Edit(User user)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ src/SimpleCRUD/Models/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleCRUD.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public sealed class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();  
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=test;Username=postgres;Password=root");
+    }
+}
